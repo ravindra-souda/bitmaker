8697cc79e70c3d70adef13d729bf6e6931595248
RAVINDRA Soudakar <ravindra.soudakar@free.fr>
2022-02-23 11:14:15 +0100

    feat: PATCH /bands

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,9 @@ const bandController = require('../api/band')
 router.post('/bands', (req, res) => {
   bandController.post(req, res)
 })
+router.patch('/bands/:key', (req, res) => {
+  bandController.patch(req, res)
+})
 router.delete('/bands/:key', (req, res) => {
   bandController.delete(req, res)
 })
