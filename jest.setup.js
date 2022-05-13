7f73999e5925fc6f48889efc52c33892248dcc38
RAVINDRA Soudakar <ravindra.soudakar@free.fr>
2022-05-13 11:36:18 +0200

    feat: POST /songs

--- jest.setup.js
+++ jest.setup.js
@@ -4,6 +4,7 @@ dotenv.config({ path: '.env.test' })
 
 const { Album } = require('./api/models/Album')
 const Band = require('./api/models/Band')
+const Song = require('./api/models/Song')
 const connect = require('./api/helpers/connect')
 
 module.exports = async () => {
@@ -14,6 +15,9 @@ module.exports = async () => {
     if (err) console.log(err)
   })
   await Album.deleteMany({}, (err) => {
+    if (err) console.log(err)
+  })
+  await Song.deleteMany({}, (err) => {
     if (err) console.log(err)
   })
 }
